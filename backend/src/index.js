d1e2c3f1a467fbc9abde2ed05d462a598661a6ed
parth2187 <parthkawatra@hotmail.com>
2022-01-17 22:23:52 +0530

    database initialization and setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 3000;
+
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
+
+app.get("/helloworld", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-    console.log(`Port listening at http://localhost:${port}`)
-})
+  console.log(`Port listening at http://localhost:${port}`);
+});
